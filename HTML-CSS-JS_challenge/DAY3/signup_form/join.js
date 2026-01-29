fc3f437f670c72a3e63d373f626c816ea3656f21
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-01-29 10:49:37 +0900

    js 과제

---
+++ HTML-CSS-JS_challenge/DAY3/signup_form/join.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#form");
+
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+
+    let userId = event.target.id.value;
+    let userPassword = event.target.password.value;
+    let userPassword1 = event.target.password1.value;
+    let userName = event.target.name.value;
+    let userPhone = event.target.phone.value;
+    let userPosition = event.target.position.value;
+    let useGender = event.target.gender.value;
+    let userEmail = event.target.email.value;
+    let userIntro = event.target.intro.value;
+
+
+    if(userId.length<6){
+        alert("아이디가 너무 짧습니다.6자 이상 입력해주세요.")
+        return 
+    }
+
+    if(userPassword !== userPassword1){
+        alert("비밀번호가 일치하지 않습니다.")
+        return 
+    }
+
+    // 가입 잘 되었다! 환영!
+    document.body.innerHTML = ""
+    document.write(`<p>${userId}님 환영합니다</p>`)
+
+
+
+})
