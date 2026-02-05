7a2577c055e261d0a31474c08b2bbce8f90e0407
Di-Mo-De-OH <dhelahep0115@gmail.com>
2026-02-05 18:28:56 +0900

    flask day2

--- flask_challenge/day2/api.py
+++ flask_challenge/day2/api.py
@@ -6,7 +6,7 @@ blp = Blueprint("books","books",url_prefix = "/book",description ="book")
 
 books = []
 
-
+id = 1
 
 @blp.route("/")
 class BookList(MethodView):
@@ -17,31 +17,34 @@ class BookList(MethodView):
     @blp.response(201,description="book add")
     @blp.arguments(BookSchemas)
     def post(self,new_book):
-        books.append(new_book)
-        return new_book
+        global id
+        book = {"book_id":id,**new_book}
+        id +=1
+        books.append(book)
+        return book
     
-@blp.route("/<string:title>")
+@blp.route("/<int:book_id>")
 class Book(MethodView):
     @blp.response(200)
-    def get(self,title):
-        book = next((book for book in books if book["title"]==title),None)
+    def get(self,book_id):
+        book = next((book for book in books if book["book_id"]==book_id),None)
         if book is None:
-            return abort(404,message = "book not found")
+            abort(404,message = "book not found")
         return book
     
     @blp.response(200)
     @blp.arguments(BookSchemas)
-    def put(self,update_book,title):
-        book = next((book for book in books if book["title"]==title),None)
+    def put(self,update_book,book_id):
+        book = next((book for book in books if book["book_id"]==book_id),None)
         if book is None:
-            return abort(404,message = "not found")
+            abort(404,message = "not found")
         book.update(update_book)
-        return update_book
+        return book
     
     @blp.response(204)
-    def delete(self,title):
+    def delete(self,book_id):
         global books
-        if not any(book for book in books if book["title"]== title):
+        if not any(book for book in books if book["book_id"]== book_id):
             abort(404,message = "book nor found")
-        books = [book for book in books if book["title"] != title ]
+        books = [book for book in books if book["book_id"] != book_id ]
         return ""
